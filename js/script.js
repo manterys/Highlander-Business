003fcec85e0ed0eda170073602c049b469730b96
manterys <see2121@gmail.com>
2023-02-06 16:30:14 +0100

    Initial commit

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const appLoader = function() {
 
     const page = document.querySelector('.page');
     const loader = document.querySelector('.loader');
+    const animationHero = document.querySelectorAll('.animation-hero');
     
     window.addEventListener('load', function() {
         loader.classList.add('loaded')
@@ -10,6 +11,9 @@ const appLoader = function() {
         loader.addEventListener('transitionend', function() {
             page.classList.add('loaded')
             loader.style.display = 'none'
+            animationHero.forEach(el => {
+                el.classList.add('animationShow')
+            })
             })
        
     })
